2d06a7a84959efd0161671e3ec4490c3ab65dc98
vnvasquez <vnvasquez@berkeley.edu>
2022-06-08 14:29:37 -0700

    update decision model

--- src/decisionmodel.jl
+++ src/decisionmodel.jl
@@ -12,13 +12,13 @@ plot_decision_ridl_females(soldec0)
 soldec3 = solve_decision_model3(prob, TargetPercentageByDate; wildtype=wild_gene,
     percent_suppression=.40, target_timestep=200) 
 
-# results 
+# results of custom objective
 results3 = format_decision_model_results(soldec3)
 sum(results3[:node_1_organism_1_control_M].control_M_G3)
 plot(results3[:node_1_organism_1_control_M].control_M_G3)
 
 # compare decision and dynamic model output 
 times, values = get_release_data(results3[:node_1_organism_1_control_M].control_M_G3)
-releases = Release(node, species, Male, 3, times, values);                                                  
+releases = Release(node, species, Male, release_gene, times, values);                                                  
 soldyn_opt1 = solve_dynamic_model(node, [releases], solver, tspan);  
 results_optimalsched1 = format_dynamic_model_results(node, soldyn_opt1)
