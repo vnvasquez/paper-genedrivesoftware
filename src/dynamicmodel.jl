3695aca834fd9abeb3bb9e6f1a815a1697e8c464
vnvasquez <vnvasquez@berkeley.edu>
2022-06-08 14:29:46 -0700

    update dynamic model

--- src/dynamicmodel.jl
+++ src/dynamicmodel.jl
@@ -1,4 +1,8 @@
+# define solution method, solve 
 tspan = (1,365)                                               
 solver = OrdinaryDiffEq.Tsit5()                               
-soldyn = solve_dynamic_model(node, solver, tspan);           
+soldyn = solve_dynamic_model(node, solver, tspan);  
+
+# results 
 results = format_dynamic_model_results(node, soldyn) 
+plot_dynamic_ridl_females(node, soldyn_opt)
