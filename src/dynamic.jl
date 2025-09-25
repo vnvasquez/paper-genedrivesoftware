29076c490f1639288b607b2a9e3539a8e0476ddb
vnvasquez <vnvasquez@berkeley.edu>
2025-09-25 14:25:43 -0700

    Inlcude baseline plots, new save function

--- src/dynamic.jl
+++ src/dynamic.jl
@@ -16,9 +16,15 @@ soldyn_histMED = solve_dynamic_model(node_histmed, solver, tspan);
 soldyn_futHI = solve_dynamic_model(node_futhigh, solver, tspan);
 soldyn_futMED = solve_dynamic_model(node_futmed, solver, tspan);
 
+# include baseline dynamic figure (Fig5a, Fig5b)
+title_Fig5a = "Fig5a: Historical Temperature Regimes (Natural Dynamics)"
+Fig5ab_dynamics(node, soldyn_histMED, soldyn_histHI, title_Fig5a, tspan)
+
+title_Fig5b = "Fig5b: Future Temperature Regimes (Natural Dynamics)"
+Fig5ab_dynamics(node, soldyn_futMED, soldyn_futHI, title_Fig5b, tspan)
+
 # grab control schedules prescribed by optimization 
 ############################################################################################
-#avgresults2000_target1 = read_csvs_to_dict(output_path, "histavgresults_target1")
 # hist det
 times1, values1 =
     get_release_data(avgresults2000_target1[:node_1_organism_1_control_M].control_M_G3);
@@ -50,49 +56,36 @@ newreleases_fut =
 ############################################################################################
 # hist det 
 soldyn_1med = solve_dynamic_model(node_histmed, [releases1], solver, tspan);
-res_soldyn1med = format_dynamic_model_results(node_histmed, soldyn_1med);
-write_dynamicdict_to_csv(res_soldyn1med, "res_soldyn1med", output_path)
-# Access dynamic results by, e.g.: 
-# read_csvs_to_dict(output_path, "res_soldyn1med")
-# res_soldyn1med['AedesAegypti"]
+save_dynamic_solution(node_histmed, soldyn_histMED, "res_soldyn1med", output_path)
 
 soldyn_1hi = solve_dynamic_model(node_histhigh, [releases1], solver, tspan);
-res_soldyn1hi = format_dynamic_model_results(node_histhigh, soldyn_1hi)
-write_dynamicdict_to_csv(res_soldyn1hi, "res_soldyn1hi", output_path)
+save_dynamic_solution(node_histhigh, soldyn_1hi, "res_soldyn1hi", output_path)
 
 # hist stoch
 soldyn_2med = solve_dynamic_model(node_histmed, [releases2], solver, tspan);
-res_soldyn2med = format_dynamic_model_results(node_histmed, soldyn_2med);
-write_dynamicdict_to_csv(res_soldyn2med, "res_soldyn2med", output_path)
+save_dynamic_solution(node_histmed, soldyn_2med, "res_soldyn2med", output_path)
 
 soldyn_2hi = solve_dynamic_model(node_histhigh, [releases2], solver, tspan);
-res_soldyn2hi = format_dynamic_model_results(node_histhigh, soldyn_2hi)
-write_dynamicdict_to_csv(res_soldyn2hi, "res_soldyn2hi", output_path)
-
-# fut det
-soldyn_3med = solve_dynamic_model(node_futmed, [releases3], solver, tspan);
-res_soldyn3med = format_dynamic_model_results(node_futmed, soldyn_3med)
-write_dynamicdict_to_csv(res_soldyn3med, "res_soldyn3med", output_path)
-
-soldyn_3hi = solve_dynamic_model(node_futhigh, [releases3], solver, tspan);
-res_soldyn3hi = format_dynamic_model_results(node_futhigh, soldyn_3hi)
-write_dynamicdict_to_csv(res_soldyn3hi, "res_soldyn3hi", output_path)
-
-# fut stoch 
-soldyn_4med = solve_dynamic_model(node_futmed, [releases4], solver, tspan);
-res_soldyn4med = format_dynamic_model_results(node_futmed, soldyn_4med)
-write_dynamicdict_to_csv(res_soldyn4med, "res_soldyn4med", output_path)
-
-soldyn_4hi = solve_dynamic_model(node_futhigh, [releases4], solver, tspan);
-res_soldyn4hi = format_dynamic_model_results(node_futhigh, soldyn_4hi)
-write_dynamicdict_to_csv(res_soldyn4hi, "res_soldyn4hi", output_path)
+save_dynamic_solution(node_histhigh, soldyn_2hi, "res_soldyn2hi", output_path)
 
 # prob hist stoch 
 newsoldyn_2hi = solve_dynamic_model(node_histhigh, [newreleases_hist], solver, tspan);
-newres_soldyn2hi = format_dynamic_model_results(node_histhigh, newsoldyn_2hi)
-write_dynamicdict_to_csv(newres_soldyn2hi, "newres_soldyn2hi", output_path)
+save_dynamic_solution(node_histhigh, newsoldyn_2hi, "newres_soldyn2hi", output_path)
+
+# fut det
+soldyn_3med = solve_dynamic_model(node_futmed, [releases3], solver, tspan);
+save_dynamic_solution(node_futmed, soldyn_3med, "res_soldyn3med", output_path)
+
+soldyn_3hi = solve_dynamic_model(node_futhigh, [releases3], solver, tspan);
+save_dynamic_solution(node_futhigh, soldyn_3hi, "res_soldyn3hi", output_path)
+
+# fut stoch 
+soldyn_4med = solve_dynamic_model(node_futmed, [releases4], solver, tspan);
+save_dynamic_solution(node_futmed, soldyn_4med, "res_soldyn4med", output_path)
+
+soldyn_4hi = solve_dynamic_model(node_futhigh, [releases4], solver, tspan);
+save_dynamic_solution(node_futhigh, soldyn_4hi, "res_soldyn4hi", output_path)
 
 # prob fut stoch 
 newsoldyn_4hi = solve_dynamic_model(node_futhigh, [newreleases_fut], solver, tspan);
-newres_soldyn4hi = format_dynamic_model_results(node_futhigh, newsoldyn_4hi)
-write_dynamicdict_to_csv(newres_soldyn4hi, "newres_soldyn4hi", output_path)
+save_dynamic_solution(node_futhigh, newsoldyn_4hi, "newres_soldyn4hi", output_path)
